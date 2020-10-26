7c998dc8e884f42341be2746dced9870eb8b6feb
amitaysoffer <amitaysof@gmail.com>
2020-10-26 22:38:35 +0000

    first card

--- src/pages/search/DisplayTweets.js
+++ src/pages/search/DisplayTweets.js
@@ -1,16 +1,35 @@
 import React from 'react'
+var FontAwesome = require('react-fontawesome')
 
 export default function DisplayTweets(props) {
   const { profileImage, user, username, date, text, link, comments, retwitted, likes } = props.tweet
   return (
-    <ul>
-      <img src={profileImage} alt="image"/>
-      <li>{user}</li>
-      <li>{username}</li>
-      <li>{date}</li>
-      <li>{text}</li>
-      <li>{link}</li>
-    </ul>
-      
+    <div className="card">
+      <div className="image-wrapper">
+        <img src={profileImage} alt="image" />
+      </div>
+      <div className="content-wrapper">
+
+        <div className="first-row">
+          <span className="user">
+            {user}
+          </span>
+          <FontAwesome className="far fa-check-double" name="check" />
+          <span className="username">
+            {username}
+          </span>
+          <FontAwesome className="fas fa-paw" name="paw" />
+          <span className="date">
+            {date}
+          </span>
+        </div>
+
+        <p>{text}</p>
+        <div>{link}</div>
+        <p>{comments} {retwitted} {likes}</p>
+      </div>
+
+    </div>
+
   )
 }
