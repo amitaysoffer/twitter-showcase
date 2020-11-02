6b984a1a53bd6d96018dcb174bad55cbb2044ead
amitaysoffer <amitaysof@gmail.com>
2020-11-02 22:43:32 +0000

    commit

--- server.js
+++ server.js
@@ -1,44 +1,55 @@
 const express = require('express');
 const axios = require('axios');
-const qs = require('qs');
 
 // import tweets dummy data
-// const data = require('./twitterData');
+const data = require('./twitterData');
+const { response } = require('express');
 const app = express();
 const port = 5000;
 
-// GET request- Take the token and put it in the subsequent requests to twitter. Need to put it in the Header as an authorisation as bearer of token and then twitter is going to work. Use the parameters the user had put in the input and search for those tweets. 
+let bearer;
+let getToken = () => {
+  if (!bearer) {
+    const config = {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      auth: {
+        username: "sbZZacfAEvxKVykuX4UhiqM06",
+        password: "MSbwMMURuNpV3awcQzoGykYKDyksnNLooCNrWldDfYDgepdNLE",
+      },
+    };
+    bearer = axios
+      .post(
+        "https://api.twitter.com/oauth2/token",
+        "grant_type=client_credentials",
+        config
+      )
+      .then((response) => {
+        // console.log(response.data.access_token);
+        return response.data.access_token;
+      })
+      .catch((error) => console.log(`Something went wrong: ${error}`));
+  }
+  return bearer;
+};
 
 
-
-// POST request to token
-async function getToken() {
-
-  return await axios.request({
-    url: "https://api.twitter.com/oauth2/token",
-    method: "post",
-    auth: {
-      username: "sbZZacfAEvxKVykuX4UhiqM06",
-      password: "MSbwMMURuNpV3awcQzoGykYKDyksnNLooCNrWldDfYDgepdNLE"
-    },
-    data: qs.stringify({
-      grant_type: "client_credentials",
-    }),
+app.get('/api/tweets', async (req, res) => {
+  const token = await getToken()
+  axios({
+    method: 'get',
+    url: 'https://api.twitter.com/1.1/search/tweets.json?q=donald',
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Accept': 'application/json',
+      "Authorization": `Bearer ${token}`,
     },
   })
     .then(function (response) {
-      // console.log(response.data);
-      // console.log(response.data.access_token);
-      return response.data
-    });
-}
+      console.log(response.data)
+      res.json(response.data);
+    })
 
-
-const Token = getToken()
-console.log(Token)
+});
 
 
 
