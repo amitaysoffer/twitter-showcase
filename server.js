1b5b9418548a2e0dd61e88a71ef7e21b082e2bbd
amitaysoffer <amitaysof@gmail.comgit config --global user.email amitaysof@gmail.com>
2020-11-04 21:22:13 +0000

    update test

--- server.js
+++ server.js
@@ -41,16 +41,6 @@ app.get('/api/search', async (req, res) => {
   axios({
     method: 'get',
     url: `https://api.twitter.com/1.1/search/tweets.json?q=${queryString}&tweet_mode=extended&count=5&result_type=popular`,
-
-    // url:`https://api.twitter.com/1.1/search/tweets.json?q=&from=${queryString}`,
-
-
-    // url: `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=${queryString}&tweet_mode=extended`,
-    // url: `https://dev.twitter.com/docs/api/1/get/statuses/user_timeline?screen_name=${queryString}&include_entities=1`,
-    // url: `http://search.twitter.com/search.atom?lang=en&q=from:${queryString}`,
-    // url: `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=${queryString}&tweet_mode=extended`,
-
-
     headers: {
       "Authorization": `Bearer ${token}`,
     },
