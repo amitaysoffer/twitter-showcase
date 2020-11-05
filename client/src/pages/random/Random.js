d8695642f3957fb338c1dd4c278296e93e7a4b2a
amitaysoffer <amitaysof@gmail.comgit config --global user.email amitaysof@gmail.com>
2020-11-05 19:06:35 +0000

    added images to random

--- client/src/pages/random/Random.js
+++ client/src/pages/random/Random.js
@@ -1,17 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import './Random.css'
 import axios from 'axios'
 import DisplayTweets from '../search/DisplayTweets'
-// import './Random.css';
-
 
 function Random() {
   const [tweets, setTweets] = useState([])
-
-
   function handleRandomClick(e) {
     e.preventDefault()
     const randomTweet = e.target.parentElement.id
-    
     axios({
       method: 'get',
       url: `api/random/?string=${randomTweet}`,
@@ -30,24 +26,29 @@ function Random() {
   return (
     <div id="random-container">
       <div className="random-card premier-league" id="plcomms">
+        <img src="https://images.unsplash.com/photo-1518605125802-006dcc629439?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="football" />
         <h1>Premier League</h1>
         <button onClick={handleRandomClick} >Tweet!</button>
       </div>
       <div className="random-card nba" id="nba">
+        <img src="https://images.unsplash.com/photo-1570840584974-9078b1d976c1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="" />
         <h1>NBA</h1>
         <button onClick={handleRandomClick} >Tweet!</button>
       </div>
       <div className="random-card theoffice" id="theofficetv">
+        <img src="https://images.unsplash.com/photo-1552318415-cc99d956c134?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="the office" />
         <h1>The Office</h1>
         <button onClick={handleRandomClick} >Tweet!</button>
       </div>
-      <div className="random-card steve-carell" id="SJobs_Stories">
-        <h1>Steve Jobs</h1>
+      <div className="random-card steve-carell" id="usainbolt">
+        <img src="https://images.unsplash.com/photo-1604454970258-811e3f92cd23?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="usainbolt" />
+        <h1>Usain Bolt</h1>
         <button onClick={handleRandomClick} >Tweet!</button>
       </div>
-      <div className="random-card spurs" id="spursofficial">
-        <h1>Spurs</h1>
-        <button onClick={handleRandomClick} >Tweet!</button>
+      <div className="random-card spurs" id="NFL" >
+        <img src="https://images.unsplash.com/photo-1566577739112-5180d4bf9390?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="nfl"/>
+        <h1>NFL</h1>
+        <button onClick={handleRandomClick}>Tweet!</button>
       </div>
       <div id="tweets">
         {tweets.map(tweet =>
