60e53a864963192220c10041c1522389c0caad6a
amitaysoffer <amitaysof@gmail.com>
2020-11-09 22:58:57 +0000

    app ready for review

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -16,7 +16,7 @@ export default function Home() {
       </div>
       <div className="app-description">
         <p>This app allows you to retreive tweets from your favourite people without having an account on twitter.
-        Don't have a person in your mind ? Go to our random section to inspire you! It presents people and organizations I personally like.</p>
+        Don't have a person in your mind ? Go to our random section and click on one of showcases. Each click you will get a random tweet from that spesific showcase.</p>
       </div>
     </div>
   )
