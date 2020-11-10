4653ffbc3edf42259e1251e961d4fb7d6f4b90b7
amitaysoffer <amitaysof@gmail.com>
2020-11-10 15:55:08 +0000

    pushing app to heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Navbar from './components/navbar/Navbar'
-import Footer from './components/footer/Footer'
+import Navbar from './staticComponents/navbar/Navbar'
+import Footer from './staticComponents/footer/Footer'
 import Home from './pages/home/Home'
 import Search from './pages/search/Search'
 import Random from './pages/random/Random'
