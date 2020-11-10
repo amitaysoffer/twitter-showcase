05495ef7405e9dc6fa93a691fe0898e710717d94
amitaysoffer <amitaysof@gmail.com>
2020-11-10 17:15:18 +0000

    changed port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-// const port = procees.env.PORT || 5000;
-const port = 5000;
+const port = process.env.PORT || 5000;
+// const port = 5000;
 const axios = require('axios');
 require('dotenv').config();
 
